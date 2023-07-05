4ae26c8bb926008fc5dcc923bc93eed0d4319622
Ambuj-coder1997 <96371080+Ambuj-coder1997@users.noreply.github.com>
2023-07-05 15:38:30 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+hide_github_icon = """
+#GithubIcon {
+  visibility: hidden;
+}
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
+page = st.sidebar.selectbox("Predict/Contributors", ("Predict", "Contributors"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
